18840ead490fb8960ee39e569ec3e1623b5741a6
Lucca de Sena Barbosa <luccasena22@gmail.com>
2025-06-29 14:51:37 -0300

    deletando arquivos repetidos.

--- main.py
+++
@@ -1,9 +0,0 @@
-from crews.content_crew.content_crew import FinancialCrew
-
-def crew_ai_project(prompt):
-
-    crew_instance = FinancialCrew()
-
-    result = crew_instance.crew().kickoff(inputs={"prompt": prompt})
-
-    return result
